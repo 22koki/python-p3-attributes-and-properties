76748d474be7fe1f4454b4a00c33e65ea44a11e8
fay <wahomeimani@gmail.com>
2023-11-27 15:43:33 +0300

    complete

--- lib/testing/dog_test.py
+++ lib/testing/dog_test.py
@@ -8,10 +8,56 @@ from lib.dog import Dog
 class TestDog(unittest.TestCase):
     '''Dog in dog.py'''
 
+    def test_is_class(self):
+        '''is a class with the name "Dog".'''
+        fido = Dog()
+        self.assertEqual(type(fido), Dog)
+
     def test_name_not_empty(self):
         '''prints "Name must be string between 1 and 25 characters." if empty string.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
         Dog(name="")
         sys.stdout = sys.__stdout__
-        print("Actual Output:", captured_out.getvalue())  # Add this line for debugging
+        expected_output = "Name must be a string between 1 and 25 characters.\n"
+        self.assertIn(expected_output, captured_out.getvalue())
+
+    def test_name_string(self):
+        '''prints "Name must be string between 1 and 25 characters." if not string.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        Dog(name=123)
+        sys.stdout = sys.__stdout__
+        expected_output = "Name must be a string between 1 and 25 characters.\n"
+        self.assertIn(expected_output, captured_out.getvalue())
+
+    def test_name_under_25(self):
+        '''prints "Name must be string between 1 and 25 characters." if string over 25 characters.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        Dog(name="What do dogs do on their day off? Can't lie around - that's their job.")
+        sys.stdout = sys.__stdout__
+        expected_output = "Name must be a string between 1 and 25 characters.\n"
+        self.assertIn(expected_output, captured_out.getvalue())
+
+    def test_valid_name(self):
+        '''saves name if string between 1 and 25 characters.'''
+        fido = Dog(name="Fido")
+        self.assertEqual(fido.name, "Fido")
+
+    def test_breed_not_in_list(self):
+        '''prints "Breed must be in list of approved breeds." if not in breed list.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        Dog(breed="Human")
+        sys.stdout = sys.__stdout__
+        expected_output = "Breed must be in the list of approved breeds.\n"
+        self.assertIn(expected_output, captured_out.getvalue())
+
+    def test_breed_in_list(self):
+        '''saves breed if in breed list.'''
+        fido = Dog(breed="Poodle")
+        self.assertEqual(fido.breed, "Poodle")
+
+if __name__ == '__main__':
+    unittest.main()
--- lib/testing/person_test.py
+++ lib/testing/person_test.py
@@ -8,7 +8,10 @@ from lib.person import Person
 class TestPerson(unittest.TestCase):
     '''Person in person.py'''
 
-    # ... other test cases ...
+    def test_is_class(self):
+        '''is a class with the name "Person".'''
+        guido = Person(name="Guido", job="Engineer")
+        self.assertEqual(type(guido), Person)
 
     def test_name_not_empty(self):
         '''prints "Name must be string between 1 and 25 characters." if empty string.'''
@@ -19,4 +22,42 @@ class TestPerson(unittest.TestCase):
         expected_output = "Name must be a string under 25 characters.\n"
         self.assertIn(expected_output, captured_out.getvalue())
 
-    # ... other test cases ...
+    def test_name_string(self):
+        '''prints "Name must be string between 1 and 25 characters." if not string.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        Person(name=123, job="Teacher")
+        sys.stdout = sys.__stdout__
+        expected_output = "Name must be a string under 25 characters.\n"
+        self.assertIn(expected_output, captured_out.getvalue())
+
+    def test_name_under_25(self):
+        '''prints "Name must be string between 1 and 25 characters." if string over 25 characters.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        Person(name="What do people do on their day off? Can't lie around - that's their job.", job="Doctor")
+        sys.stdout = sys.__stdout__
+        expected_output = "Name must be a string under 25 characters.\n"
+        self.assertIn(expected_output, captured_out.getvalue())
+
+    def test_valid_name(self):
+        '''saves name if string under 25 characters.'''
+        alice = Person(name="Alice", job="Artist")
+        self.assertEqual(alice.name, "Alice")
+
+    def test_job_not_in_list(self):
+        '''prints "Job must be in list of approved jobs." if not in job list.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        Person(name="Bob", job="Plumber")
+        sys.stdout = sys.__stdout__
+        expected_output = "Job must be in the list of approved jobs.\n"
+        self.assertIn(expected_output, captured_out.getvalue())
+
+    def test_job_in_list(self):
+        '''saves job if in job list.'''
+        bob = Person(name="Bob", job="Engineer")
+        self.assertEqual(bob.job, "Engineer")
+
+if __name__ == '__main__':
+    unittest.main()
